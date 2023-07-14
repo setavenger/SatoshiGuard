28673517b2e8c064780d166c84dafe6288abd89e
setavenger <setor.blagogee@gmx.de>
2023-07-14 10:57:44 +0200

    Initial Commit

---
+++ SatoshiGuard/SatoshiGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatoshiGuardApp.swift
+//  SatoshiGuard
+//
+//  Created by Setor Blagogee on 14.07.23.
+//
+
+import SwiftUI
+
+@main
+struct SatoshiGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SatoshiGuardUITests/SatoshiGuardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SatoshiGuardUITestsLaunchTests.swift
+//  SatoshiGuardUITests
+//
+//  Created by Setor Blagogee on 14.07.23.
+//
+
+import XCTest
+
+final class SatoshiGuardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
